b2ce22270f0b620dbf151857d3c60666dccc10d0
pablo <pablochavez9817@gmail.com>
2023-11-23 20:36:26 -0600

    proyecto final progra 2

---
+++ src/main/java/com/example/proyectofinalprograiimvc/repositorio/ClienteRepositorio.java
@@ -0,0 +1,9 @@
+package com.example.proyectofinalprograiimvc.repositorio;
+
+import com.example.proyectofinalprograiimvc.modelo.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepositorio extends JpaRepository<Cliente, Long> {
+    Cliente findByUsuarioId(Long id);
+    Cliente findByCui(String cui);
+}
